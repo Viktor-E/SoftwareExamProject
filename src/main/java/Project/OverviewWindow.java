8c82396c8c50ab4cdc46b307d78fc0ce87867899
Michael Bendtsen <mich.bendtsen@gmail.com>
2022-05-09 22:36:43 +0200

    OverviewWindow won't stop the program

--- src/main/java/Project/OverviewWindow.java
+++ src/main/java/Project/OverviewWindow.java
@@ -6,7 +6,8 @@ public class OverviewWindow extends Window {
 
     OverviewWindow(Program program) throws ParseException {
         super("Overview Window\n Welcome " + program.currentUser.getName() + "\nselect new Window:\n 1: Register Time \n 2: Project\n 3: Manage Projects\n 4: Log ud", program);
+        while(true){
         changeWindow();
-    }
+    }}
 
 }
